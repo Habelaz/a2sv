a3944cfcb998b2c627712b68003fbc631af6c05d
Habelaz <123306599+Habelaz@users.noreply.github.com>
2023-10-18 16:38:25 +0300

    Time: 42 ms (78.06%) | Memory: 16.3 MB (84.48%) - LeetSync

--- 415-add-strings/add-strings.py
+++ 415-add-strings/add-strings.py
@@ -1,17 +1,9 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
-        carry = 0
-        result = []
-        i, j = len(num1) - 1, len(num2) - 1
+        sys.set_int_max_str_digits(10000)
+        n=num1
+        n=int(n)
+        n1=num2
+        n1=int(n1)
 
-        while i >= 0 or j >= 0 or carry:
-            digit1 = int(num1[i]) if i >= 0 else 0
-            digit2 = int(num2[j]) if j >= 0 else 0
-            total = digit1 + digit2 + carry
-            carry = total // 10
-            current_digit = total % 10
-            result.insert(0, str(current_digit))
-            i -= 1
-            j -= 1
-
-        return ''.join(result)
+        return str(n+n1)
