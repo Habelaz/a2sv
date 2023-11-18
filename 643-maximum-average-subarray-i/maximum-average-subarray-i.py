6002af04edab0058cbd375af838b669c4756b5e7
Habelaz <123306599+Habelaz@users.noreply.github.com>
2023-11-18 11:31:48 +0300

    Time: 1018 ms (51.30%) | Memory: 28.1 MB (19.39%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.py
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.py
@@ -1,14 +1,10 @@
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        if k > len(nums):
-            raise ValueError("Subarray length k cannot be greater than array length")
-
-        max_average = sum(nums[:k]) / k
-        current_sum = sum(nums[:k])
-
-        for i in range(k, len(nums)):
-            current_sum += nums[i] - nums[i - k]
-            current_average = current_sum / k
-            max_average = max(max_average, current_average)
-
-        return max_average
+        n = len(nums)
+        s = sum(nums[:k])
+        answer = s
+        for i in range(k, n):
+            s += nums[i]
+            s -= nums[i-k]
+            answer = max(answer, s)
+        return answer/k
