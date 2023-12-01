f7a8adacfd56e1cec8a25e86231fc85aac43de41
Habelaz <123306599+Habelaz@users.noreply.github.com>
2023-12-01 17:32:06 +0300

    Time: 46 ms (83.67%) | Memory: 16.4 MB (24.22%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,15 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        myList=[char for char in s]
+        myList.reverse()
+        total=0
+        pre=0
+        for i in myList:
+            value=roman_dict[i]
+            if value<pre:
+                total -=value
+            else:
+                total +=value
+            pre=value
+        return total
