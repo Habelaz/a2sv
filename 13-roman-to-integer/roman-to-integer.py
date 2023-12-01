91e39205a2e0893c48ecea9eeed5719771efc940
Habelaz <123306599+Habelaz@users.noreply.github.com>
2023-12-01 17:34:04 +0300

    Time: 55 ms (39.20%) | Memory: 16.4 MB (24.22%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,7 +1,9 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
         roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        myList=[char for char in s]
+        myList=[]
+        for c in s:
+            myList.append(c)
         myList.reverse()
         total=0
         pre=0
