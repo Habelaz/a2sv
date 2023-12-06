d9a23e79830faa6188be51387d0585ff4395fce8
Habelaz <123306599+Habelaz@users.noreply.github.com>
2023-12-06 17:51:50 +0300

    Time: 119 ms (18.81%) | Memory: 17.8 MB (37.77%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -2,8 +2,9 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
         ans = []
         n = len(nums)/3
+
         for i in nums:
-            if nums.count(i) > n:
-                if i not in ans:
+            if i not in ans:
+                if nums.count(i) > n:
                     ans.append(i)
         return ans
