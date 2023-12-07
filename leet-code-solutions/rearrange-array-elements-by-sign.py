46029ba6dcef45ab82cfe9cfafe931023b27f9b5
Habelaz <123306599+Habelaz@users.noreply.github.com>
2023-12-07 15:23:33 +0300

    Added solution for Rearrange Array Elements by Sign

--- leet-code-solutions/rearrange-array-elements-by-sign.py
+++ leet-code-solutions/rearrange-array-elements-by-sign.py
@@ -1,9 +1,19 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-        pos = [num for num in nums if num > 0]
-        neg = [num for num in nums if num < 0]
-        ans = []
-        for i in range(len(pos)):
-            ans.append(pos[i])
-            ans.append(neg[i])
+        # pos = [num for num in nums if num > 0]
+        # neg = [num for num in nums if num < 0]
+        # ans = []
+        # for i in range(len(pos)):
+        #     ans.append(pos[i])
+        #     ans.append(neg[i])
+        # return ans
+        l,r = 0,1
+        ans = [None] * len(nums)
+        for num in nums:
+            if num > 0:
+                ans[l] = num
+                l += 2
+            else:
+                ans[r] = num
+                r += 2
         return ans
