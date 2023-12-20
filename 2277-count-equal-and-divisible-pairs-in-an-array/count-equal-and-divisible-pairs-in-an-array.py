21add24fa763f35f867e572b36c3303bb88e7afd
Habelaz <123306599+Habelaz@users.noreply.github.com>
2023-12-20 17:49:07 +0300

    Time: 73 ms (82.49%) | Memory: 17 MB (6.17%) - LeetSync

--- 2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.py
+++ 2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.py
@@ -3,9 +3,6 @@ class Solution:
         count = 0
         if len(set(nums)) == len(nums):
             return 0
-        # i,j = 0,len(nums)-1
-        # while i<j:
-        #     if nums[i] == nums[j] and (i*j)%k == 0:
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
                 if nums[i] == nums[j] and (i*j)%k == 0:
