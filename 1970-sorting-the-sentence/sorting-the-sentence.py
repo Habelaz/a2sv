7ed88a11b0a469c3e253b5b65fb5583fd17e0064
Habelaz <123306599+Habelaz@users.noreply.github.com>
2023-12-29 14:35:26 +0300

    Time: 58 ms (5.53%) | Memory: 17.3 MB (7.03%) - LeetSync

---
+++ 1970-sorting-the-sentence/sorting-the-sentence.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sortSentence(self, s: str) -> str:
+        s = list(s.split())
+        ans = [None]*len(s)
+        for i in s:
+            curr = int(i[-1])
+            ans[curr - 1] = i[:-1]
+            # print(ans)
+        return ' '.join(ans)
