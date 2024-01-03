1e5ad57b58e8cfdfc8d1aec688f041e84e1b2a8b
Habelaz <123306599+Habelaz@users.noreply.github.com>
2024-01-03 11:29:02 +0300

    Time: 105 ms (5.07%) | Memory: 22.7 MB (5.52%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,7 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        clean = [char.lower() for char in s if char.isalnum()]
-        clean = ''.join(clean)
+        clean = ''.join(char.lower() for char in s if char.isalnum())
         if clean == clean[::-1]:
             return True
         else:
