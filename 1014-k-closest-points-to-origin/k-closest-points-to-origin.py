764cdd53335194fe6102df818db0caf3e8767606
Habelaz <123306599+Habelaz@users.noreply.github.com>
2024-01-05 15:26:16 +0300

    Time: 563 ms (98.94%) | Memory: 24 MB (6.19%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -7,7 +7,7 @@ class Solution:
         # ans = [mydict[key] for key in sorted(mydict.keys())]
         # if len(points) == k:
         #     return points
-        points = sorted(points,key= lambda x : x[0]**2 + x[1]**2)
-        return points[:k]
+        return sorted(points,key= lambda x : x[0]**2 + x[1]**2)[:k]
+        
         
         
