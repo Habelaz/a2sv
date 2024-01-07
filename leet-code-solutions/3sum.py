21f5a3967d1f67fe8a01ea0a35949bcc20b80453
Habelaz <123306599+Habelaz@users.noreply.github.com>
2024-01-07 21:43:02 +0300

    Added solution for 3Sum

---
+++ leet-code-solutions/3sum.py
@@ -0,0 +1,26 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        result = set()  # Use a set to store unique triplets
+        
+        for i in range(len(nums)):
+            if i > 0 and nums[i-1] == nums[i]:
+                continue
+            
+            j = i + 1
+            k = len(nums) - 1
+            
+            while j < k:
+                s = nums[i] + nums[j] + nums[k]
+                
+                if s > 0:
+                    k -= 1
+                elif s < 0:
+                    j += 1
+                else:
+                    result.add((nums[i], nums[j], nums[k]))
+                    j += 1
+                    while j < k and nums[j-1] == nums[j]:
+                        j += 1
+                        
+        return list(result)
