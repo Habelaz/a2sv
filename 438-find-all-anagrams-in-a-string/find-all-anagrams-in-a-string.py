dcd90cbd189b650db0dab0e96daf9af361aae4a3
Habelaz <123306599+Habelaz@users.noreply.github.com>
2024-01-09 18:03:28 +0300

    Time: 5070 ms (15.32%) | Memory: 18.7 MB (7.52%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,12 +1,14 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
+        map1 = Counter(p)
+        l = 0
         k = len(p)
         ans = []
-        target = sorted(p)  # Sort the target string to compare with later
-
-        for i in range(len(s) - k + 1): 
-            window = sorted(s[i:i+k])  
-            if window == target:  
-                ans.append(i)
-
+        for r in range(k,len(s)+1):
+            wind = s[l:r]
+            map2 = Counter(wind)
+            if map1 == map2:
+                ans.append(l)
+                print(l)
+            l += 1
         return ans
