b847bc64af7e2e6dbc1706bec0403761eb79ab8c
Habelaz <123306599+Habelaz@users.noreply.github.com>
2024-01-11 18:11:30 +0300

    Time: 849 ms (96.61%) | Memory: 31.5 MB (12.49%) - LeetSync

--- 1813-maximum-erasure-value/maximum-erasure-value.py
+++ 1813-maximum-erasure-value/maximum-erasure-value.py
@@ -3,9 +3,9 @@ class Solution:
         l = 0
         ans = set()
         max_s = 0
-        
+        summ = 0
         for r in range(len(nums)):
-            summ = sum(ans)
+            
             while nums[r] in ans:
                 ans.remove(nums[l])
                 summ -= nums[l]
