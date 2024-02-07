18d7ff1f04b945d628b9c197fbc27f8545df7a55
Habelaz <123306599+Habelaz@users.noreply.github.com>
2024-02-07 16:37:20 +0300

    Add solution for Shifting Letters II

---
+++ leet_code_solutions/leetcode/shifting-letters-ii.py
@@ -0,0 +1,27 @@
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+        sh =[0] * (len(s)+1)
+        for i in range(len(shifts)):
+            di = shifts[i][2]
+            a = shifts[i][0]
+            b = shifts[i][1]
+            if di == 0:
+                sh[a] -= 1
+                sh[b+1] += 1
+            else:
+                sh[a] += 1
+                sh[b+1] -= 1
+        
+        acc = 0
+        for i in range(len(sh)):
+            acc += sh[i]
+            sh[i] = acc
+
+        alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+        ans = ''
+        for i in range(len(s)):
+            ind = alpha.index(s[i]) + sh[i]
+            ans += alpha[ind%26]
+        
+        return ans
+
