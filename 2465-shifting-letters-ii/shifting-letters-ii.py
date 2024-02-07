4b5ca17b774d61805e9e6f375dfcca46e2c843d0
Habelaz <123306599+Habelaz@users.noreply.github.com>
2024-02-07 16:49:50 +0300

    Time: 1043 ms (43.53%) | Memory: 42.3 MB (47.35%) - LeetSync

--- 2465-shifting-letters-ii/shifting-letters-ii.py
+++ 2465-shifting-letters-ii/shifting-letters-ii.py
@@ -17,11 +17,14 @@ class Solution:
             acc += sh[i]
             sh[i] = acc
 
-        alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+        # alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
         ans = ''
-        for i in range(len(s)):
-            ind = alpha.index(s[i]) + sh[i]
-            ans += alpha[ind%26]
+        for i,c in enumerate(s):
+            # ind = alpha.index(s[i]) + sh[i]
+            # ans += alpha[ind%26]
+
+            ind = (ord(c) - ord('a') + sh[i]) % 26
+            ans += chr(ind + ord('a'))
         
         return ans
 
