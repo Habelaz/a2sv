aaf432f297b1cfb5912a26e3bd1493761954d1d7
Habelaz <123306599+Habelaz@users.noreply.github.com>
2024-02-12 16:07:28 +0300

    Add solution for Middle of the Linked List

---
+++ leet_code_solutions/leetcode/middle-of-the-linked-list.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        dummy.next = head
+        fast = head
+        slow = dummy
+        while fast and fast.next :
+            slow = slow.next
+            fast = fast.next.next
+        return slow.next
