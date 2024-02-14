f99f640e5c9aec9249c91d54f2db6441f1b18fa4
Habelaz <123306599+Habelaz@users.noreply.github.com>
2024-02-14 08:04:13 +0300

    Time: 76 ms (96.16%) | Memory: 17.4 MB (91.75%) - LeetSync

--- 3195-separate-black-and-white-balls/separate-black-and-white-balls.py
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.py
@@ -1,25 +1,15 @@
 class Solution:
     def minimumSteps(self, s: str) -> int:
-        n = len(s)
-        lis = list(s)
-        l = 0
-        r = n-1
-        swaps = 0
-        while l < r:
-            if lis[l] == "1" and lis[r] == "0":
-                lis[l],lis[r]=lis[r],lis[l]
-                
-                swaps += (r-l)
-                l += 1
-                r -= 1
-            elif lis[l] == "1" and lis[r] == "1":
-                r-=1
-            elif lis[l] == "0" and lis[r] == "0":
-                l += 1
+        blacks = 0
+        ans = 0
+
+        for i in s:
+            if i == '1':
+                blacks += 1
             else:
-                l += 1
-                r -= 1
-        return swaps
+                ans += blacks
+
+        return ans
 
             
 
