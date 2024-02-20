3ebf2e69996676751340db1c5bb2fba3446ef992
Habelaz <123306599+Habelaz@users.noreply.github.com>
2024-02-20 11:36:59 +0300

    Time: 914 ms (38.58%) | Memory: 30.1 MB (87.87%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -1,14 +1,13 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         stack = []
-        mapp = defaultdict(lambda :0)
         ans = [0] * len(temperatures)
+
         for ind,temp in enumerate(temperatures):
             while stack and temperatures[stack[-1]] < temp:
                 lower = stack.pop()
-                # print(lower)
-                # mapp[lower] = temp
                 ans[lower] = ind - lower
+            
             stack.append(ind)
-
         return ans
+        
