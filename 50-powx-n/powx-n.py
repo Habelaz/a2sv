cc50bd8356abf865b7d1c57745516e39dd4e0f2a
Habelaz <123306599+Habelaz@users.noreply.github.com>
2024-02-21 12:52:18 +0300

    Time: 32 ms (79.01%) | Memory: 16.6 MB (57.92%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,24 +1,13 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        # sys.setrecursionlimit((2**31)-1)
-        # if n >= 0:
-        #     def pow(x,n,cnt):
-        #         if n >= 0:
-        #             if cnt >= n:
-        #                 return 1
-        #             return x * pow(x,n,cnt+1)
-        #     cnt = 0
-        #     return pow(x,n,cnt)
-        # else:
-        #     x = 1/x
-        #     def nepow(x,n,cnt):
-        #         if cnt <= n:
-        #             return 1
-                
-        #         return x * nepow(x,n,cnt-1)
-        
-        #     cnt = 0
-        #     return nepow(x,n,cnt)
-        return x**(n)
-        
-            
+        def pow(x,n):
+            if n == 0:
+                return 1
+            if n == 1:
+                return x
+            if n == -1:
+                return 1/x
+
+            return (pow(x,n//2)**2) * pow(x,n%2)
+
+        return(pow(x,n))             
