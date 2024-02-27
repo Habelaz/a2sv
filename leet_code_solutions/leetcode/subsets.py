ce1cffaf81911baf3bb6d7fc4db7667e97fd0509
Habelaz <123306599+Habelaz@users.noreply.github.com>
2024-02-27 11:28:36 +0300

    Add solution for Subsets

---
+++ leet_code_solutions/leetcode/subsets.py
@@ -0,0 +1,19 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        subset = []
+        def backtrack(i):
+
+            if i <= len(nums):
+                ans.append(subset.copy())
+            else:
+                return
+                
+            for i in range(i, len(nums)):
+                
+                subset.append(nums[i])
+                backtrack(i+1)
+                subset.pop()
+        
+        backtrack(0)
+        return ans
