e5e11cf10b4a326f9d8afa0c898a6889fdabfab0
Habelaz <123306599+Habelaz@users.noreply.github.com>
2024-02-27 11:18:30 +0300

    Time: 31 ms (91.40%) | Memory: 16.6 MB (98.76%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -4,14 +4,14 @@ class Solution:
         subset = []
         def backtrack(i):
 
-            if i >= len(nums):
-                ans.append(subset[:])
-                return 
-            subset.append(nums[i])
-            backtrack(i+1)
-
-            subset.pop()
-            backtrack(i+1)
+            if i <= len(nums):
+                ans.append(subset.copy())
+                
+            for i in range(i, len(nums)):
+                
+                subset.append(nums[i])
+                backtrack(i+1)
+                subset.pop()
         
         backtrack(0)
         return ans
