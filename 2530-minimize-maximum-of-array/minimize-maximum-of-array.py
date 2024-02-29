3c5b37cb63c008429fc64e659fdb8a3a989dabc8
Habelaz <123306599+Habelaz@users.noreply.github.com>
2024-02-29 14:57:38 +0300

    Time: 570 ms (76.52%) | Memory: 29.3 MB (82.61%) - LeetSync

--- 2530-minimize-maximum-of-array/minimize-maximum-of-array.py
+++ 2530-minimize-maximum-of-array/minimize-maximum-of-array.py
@@ -6,10 +6,9 @@ class Solution:
         for i in range(len(nums)):
             acc += nums[i]
             pref[i] = acc
-        print(pref)
+
         res = nums[0]
         for i in range(len(pref)):
-            # print(math.ceil(pref[i] / (i+1)) )
             if math.ceil(pref[i] / (i+1)) > res:
                 res = ceil(pref[i]/(i+1))
         return res
