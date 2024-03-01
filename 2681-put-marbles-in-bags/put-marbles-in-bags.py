a91e3d0d6931ebc66810b1137533132e68b2776b
Habelaz <123306599+Habelaz@users.noreply.github.com>
2024-03-01 11:00:18 +0300

    Time: 556 ms (66.04%) | Memory: 29.7 MB (95.28%) - LeetSync

--- 2681-put-marbles-in-bags/put-marbles-in-bags.py
+++ 2681-put-marbles-in-bags/put-marbles-in-bags.py
@@ -1,16 +1,18 @@
 class Solution:
     def putMarbles(self, weights: List[int], k: int) -> int:
+        if k == 1:
+            return 0
+            
         pref = [0] *( len(weights) - 1)
-
         for i in range(1,len(weights)):
             pref[i-1] = weights[i] + weights[i-1]
         pref.sort()
-        # minn = sum(pref[:k-1])
-        # maxx = sum(pref[-(k-1):])
-        minn = 0
-        maxx = 0
-        for i in range(k-1):
-            minn += pref[i]
-            maxx += pref[len(pref)-i-1]
+        minn = sum(pref[:k-1])
+        maxx = sum(pref[-k+1:])
+        # minn = 0
+        # maxx = 0
+        # for i in range(k-1):
+        #     minn += pref[i]
+        #     maxx += pref[len(pref)-i-1]
         
         return maxx - minn
