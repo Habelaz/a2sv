9824de4357697347cc74b40eff3d7194cf7bd554
Habelaz <123306599+Habelaz@users.noreply.github.com>
2024-03-24 13:29:13 +0300

    Time: 70 ms (87.32%) | Memory: 17.8 MB (46.85%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,33 +1,33 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         # l,r = 0,len(nums)-1
-        def first(nums,l,r):
-            while l <= r:
-                mid = (l+r) //2
+        def first(left,right):
+            while left <= right:
+                mid = left + (right - left) // 2
                 if nums[mid] == target:
-                    if mid == 0 or nums[mid-1] != target:
+                    if mid == 0 or  nums[mid - 1] != target:
                         return mid
                     else:
-                        r = mid - 1
-                elif nums[mid] < target:
-                    l = mid + 1
+                        right = mid - 1
+                elif nums[mid] > target:
+                    right = mid - 1
                 else:
-                    r = mid - 1
+                    left = mid + 1
             return -1
 
-        def last(nums,l,r):
-            while l <= r:
-                mid = (l+r) //2
+        def last(left,right):
+            while left <= right:
+                mid = left + (right - left) // 2
                 if nums[mid] == target:
                     if mid == len(nums) - 1 or nums[mid+1] != target:
                         return mid
                     else:
-                        l = mid +1
-                elif nums[mid] < target:
-                    l = mid + 1
+                        left = mid + 1
+                elif nums[mid] > target:
+                    right = mid - 1
                 else:
-                    r = mid - 1
+                    left = mid + 1
             return -1
         
-        return [first(nums,0,len(nums)-1),last(nums,0,len(nums)-1)]
+        return [first(0,len(nums)-1),last(0,len(nums)-1)]
         
