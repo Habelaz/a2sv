30dc9e11e665157ca0f00d31ca0f01e1af841b9f
Habelaz <123306599+Habelaz@users.noreply.github.com>
2024-03-27 19:11:31 +0300

    Time: 44 ms (10.88%) | Memory: 16.5 MB (98.28%) - LeetSync

--- 263-ugly-number/ugly-number.py
+++ 263-ugly-number/ugly-number.py
@@ -1,21 +1,14 @@
 class Solution:
     def isUgly(self, n: int) -> bool:
-        primes = []
-        if n <= 0:
-            return False
-        d = 2
-        while d*d <= n:
-            while n % d == 0:
-                primes.append(d)
-                n //= d
-            d += 1
-        if n > 1:
-            primes.append(n)
-        # print(primes)
-        st = {2,3,5}
-
-        primes =set(primes)
-        for p in primes:
-            if p not in st:
+        while n >= 1:
+            if n%2 == 0:
+                n=n//2
+            elif n%3 == 0:
+                n=n//3
+            elif n%5 == 0:
+                n=n//5
+            elif n == 1:
+                return True
+            else:
                 return False
-        return True
+        
