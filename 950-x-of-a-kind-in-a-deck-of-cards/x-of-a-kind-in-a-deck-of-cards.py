7bfde5d872c6a5a706da461863764659244cfeb6
Habelaz <123306599+Habelaz@users.noreply.github.com>
2024-03-27 18:04:54 +0300

    Time: 104 ms (91.67%) | Memory: 17.1 MB (32.29%) - LeetSync

--- 950-x-of-a-kind-in-a-deck-of-cards/x-of-a-kind-in-a-deck-of-cards.py
+++ 950-x-of-a-kind-in-a-deck-of-cards/x-of-a-kind-in-a-deck-of-cards.py
@@ -1,6 +1,10 @@
 class Solution:
     def hasGroupsSizeX(self, deck: List[int]) -> bool:
         count = Counter(deck)
-        vals = count.values()
-        d = math.gcd(*vals)
+        d = 0
+        for key,val in count.items():
+            if d == 0:
+                d = val
+            else:
+                d = math.gcd(d,val)
         return True if d > 1 else False
