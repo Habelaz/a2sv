d7f0e13e965a3751c1377e5b0cfe21c73a419f73
Habelaz <123306599+Habelaz@users.noreply.github.com>
2024-03-30 07:33:57 +0300

    Time: 414 ms (23.92%) | Memory: 20 MB (97.13%) - LeetSync

--- 475-heaters/heaters.py
+++ 475-heaters/heaters.py
@@ -2,23 +2,23 @@ class Solution:
     def findRadius(self, houses: List[int], heaters: List[int]) -> int:
         radius = 0
         heaters.sort()
-        for house in houses:
+        def fun():
             low,high = 0,len(heaters) - 1
             min_d = float('inf')
-
             while low <= high:
                 mid = low + (high - low) // 2
                 if heaters[mid] == house:
                     min_d = 0
                     break
                 elif heaters[mid] < house:
-                    min_d = min(min_d,abs(heaters[mid] - house))
                     low = mid + 1
                 else:
-                    min_d = min(min_d,abs(heaters[mid] - house))
                     high = mid - 1
-            # print(low)
-            # min_d = min(min_d,abs(heaters[low]) - house)
-            radius = max(radius,min_d)
+                min_d = min(min_d,abs(heaters[mid] - house))
+            return min_d
+
+        for house in houses:
+            
+            radius = max(radius,fun())
         return radius
                     
