c4b62fca65abcc35497310648ea51e68ba77fa2a
Habelaz <123306599+Habelaz@users.noreply.github.com>
2024-04-08 15:33:55 +0300

    Time: 178 ms (85.08%) | Memory: 17.8 MB (17.17%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        visited = [False for i in range(n)] 
+        
+        def dfs(u):
+            for v in range(n):
+                if isConnected[u][v] == 1 and not visited[v]:
+                    visited[v] = True
+                    dfs(v)
+
+        count = 0
+        for i in range(n):
+            if not visited[i]:
+                visited[i] = True
+                count += 1
+                dfs(i)
+
+        return count
