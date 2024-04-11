1aeca46303db6f5c3b8c642c3b9fbcb5ee968adf
Habelaz <123306599+Habelaz@users.noreply.github.com>
2024-04-11 18:15:31 +0300

    Time: 65 ms (60.07%) | Memory: 17 MB (60.33%) - LeetSync

---
+++ 871-keys-and-rooms/keys-and-rooms.py
@@ -0,0 +1,19 @@
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        visited = [False] * len(rooms)
+        visited[0] = True
+        q = deque()
+        q.append(0)
+
+        while q:
+            room = q.popleft()
+            for k in rooms[room]:
+                if not visited[k]:
+                    visited[k] = True
+                    q.append(k)
+        visited = set(visited)
+        return not(False in visited)
+        
+
+
+        
