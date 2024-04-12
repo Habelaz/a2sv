912bb6ca495a2cd918dcd26a7d5e4a4fa924abfa
Habelaz <123306599+Habelaz@users.noreply.github.com>
2024-04-12 16:03:07 +0300

    Time: 75 ms (87.13%) | Memory: 16.8 MB (27.58%) - LeetSync

--- 1229-shortest-path-with-alternating-colors/shortest-path-with-alternating-colors.py
+++ 1229-shortest-path-with-alternating-colors/shortest-path-with-alternating-colors.py
@@ -14,21 +14,22 @@ class Solution:
         # dist = 0
         
         while q:
-            node,length,edgecolor = q.popleft()
-            if answer[node] == -1:
-                answer[node] = length
+            for i in range(len(q)):
+                node,length,edgecolor = q.popleft()
+                if answer[node] == -1:
+                    answer[node] = length
 
-            if edgecolor != 'red':
-                for neigh in red[node]:
-                    if (neigh,'red') not in visited:
-                        visited.add((neigh,'red'))
-                        q.append((neigh,length+1,'red'))
+                if edgecolor != 'red':
+                    for neigh in red[node]:
+                        if (neigh,'red') not in visited:
+                            visited.add((neigh,'red'))
+                            q.append((neigh,length+1,'red'))
 
-            if edgecolor != 'blue':
-                for neigh in blue[node]:
-                    if (neigh,'blue') not in visited:
-                        visited.add((neigh,'blue'))
-                        q.append((neigh,length+1,'blue'))
+                if edgecolor != 'blue':
+                    for neigh in blue[node]:
+                        if (neigh,'blue') not in visited:
+                            visited.add((neigh,'blue'))
+                            q.append((neigh,length+1,'blue'))
             # dist += 1
 
         return answer
