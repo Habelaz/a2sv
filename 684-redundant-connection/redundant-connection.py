713c7dd2b257a7fdb230f5f4228de58c167219f8
Habelaz <123306599+Habelaz@users.noreply.github.com>
2024-04-26 09:15:28 +0300

    Time: 57 ms (47.11%) | Memory: 17 MB (89.44%) - LeetSync

--- 684-redundant-connection/redundant-connection.py
+++ 684-redundant-connection/redundant-connection.py
@@ -6,34 +6,37 @@ class Solution:
         #     graph[b].append(a)
         rank = [0] * (len(edges)+1)
         parent = [i for i in range(len(edges)+1)]
-        def find(node):
-            while node != parent[node]:
-                parent[node] = parent[parent[node]]
-                node = parent[node]
-            return node
+        def union_find(node1,node2):
+            def find(node):
+                while node != parent[node]:
+                    parent[node] = parent[parent[node]]
+                    node = parent[node]
+                return node
 
-        def union(node1,node2):
-            parent1 = find(node1)
-            parent2 = find(node2)
-            if parent1 != parent2:
-                if rank[parent1] > rank[parent2]:
-                    parent[parent2] = parent1
-                elif rank[parent2] > rank[parent1]:
-                    parent[parent1] = parent2
+            def union(node1,node2):
+                parent1 = find(node1)
+                parent2 = find(node2)
+                if parent1 != parent2:
+                    if rank[parent1] > rank[parent2]:
+                        parent[parent2] = parent1
+                    elif rank[parent2] > rank[parent1]:
+                        parent[parent1] = parent2
 
-                else:
-                    parent[parent2] = parent1
-                    rank[parent1] += 1
-                return 0
-            else:
-                return [node1,node2]
-        
-        
+                    else:
+                        parent[parent2] = parent1
+                        rank[parent1] += 1
+
+            def is_connected(node1,node2):
+                return find(node1) == find(node2)
+            if is_connected(node1,node2):
+                return False
+            
+            union(node1,node2)
+            return True
             
 
         for a,b in edges:
-            found = union(a,b)
-            if found :
-                return found
+            if not union_find(a,b):
+                return [a,b]
 
 
