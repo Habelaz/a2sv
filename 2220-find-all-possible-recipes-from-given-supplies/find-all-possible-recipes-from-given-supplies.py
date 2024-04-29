020bfac586c9b7c7835cd0bc35e8de36fe46527a
Habelaz <123306599+Habelaz@users.noreply.github.com>
2024-04-29 14:44:03 +0300

    Time: 818 ms (43.80%) | Memory: 19.8 MB (55.60%) - LeetSync

---
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+        graph = defaultdict(list)
+        incoming = defaultdict(int)
+
+        for i in range(len(ingredients)):
+            for ing in ingredients[i]:
+                graph[ing].append(recipes[i])
+                incoming[recipes[i]] += 1
+        # print(graph)
+        q = deque(supplies)
+        ans = []
+
+        while q:
+            node = q.popleft()
+            for nbr in graph[node]:
+                incoming[nbr] -= 1
+                if incoming[nbr] == 0:
+                    q.append(nbr)
+                    ans.append(nbr)
+
+        return ans
