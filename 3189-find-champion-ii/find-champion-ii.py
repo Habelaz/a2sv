9fd0791ed6b5c1611185c838e4a85b25bb569935
Habelaz <123306599+Habelaz@users.noreply.github.com>
2024-05-02 10:05:18 +0300

    Time: 738 ms (97.31%) | Memory: 18 MB (91.94%) - LeetSync

--- 3189-find-champion-ii/find-champion-ii.py
+++ 3189-find-champion-ii/find-champion-ii.py
@@ -1,10 +1,10 @@
 class Solution:
     def findChampion(self, n: int, edges: List[List[int]]) -> int:
-        graph = defaultdict(list)
+        # graph = defaultdict(list)
         incoming = [0 for i in range(n)]
 
         for u,v in edges:
-            graph[u].append(v)
+            # graph[u].append(v)
             incoming[v] += 1
 
         ans = [i for i,inc in enumerate(incoming) if inc == 0]
