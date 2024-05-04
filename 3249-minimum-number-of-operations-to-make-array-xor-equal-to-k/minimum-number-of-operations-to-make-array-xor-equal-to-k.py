e42ced34f859464595bfecae61911a8466bbe919
Habelaz <123306599+Habelaz@users.noreply.github.com>
2024-05-04 12:05:06 +0300

    Time: 591 ms (30.78%) | Memory: 31 MB (41.09%) - LeetSync

--- 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.py
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.py
@@ -1,13 +1,14 @@
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
         
-        x = 0
-        for n in nums:
-            x ^= n
+        x = nums[0]
+        for i in range(1,len(nums)):
+            x ^= nums[i]
         x ^= k
-        ans = 0
-        while x>0:
-            if x & 1 == 1:
-                ans += 1
-            x >>= 1
-        return ans
+        # ans = 0
+        # while x>0:
+        #     if x & 1 == 1:
+        #         ans += 1
+        #     x >>= 1
+        # return ans
+        return x.bit_count()
