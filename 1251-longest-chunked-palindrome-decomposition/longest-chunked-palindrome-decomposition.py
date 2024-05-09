078a209a5a2e7de3dd681618eecc39b3ca2019cd
Habelaz <123306599+Habelaz@users.noreply.github.com>
2024-05-09 07:14:06 +0300

    Time: 38 ms (60.17%) | Memory: 16.6 MB (76.27%) - LeetSync

--- 1251-longest-chunked-palindrome-decomposition/longest-chunked-palindrome-decomposition.py
+++ 1251-longest-chunked-palindrome-decomposition/longest-chunked-palindrome-decomposition.py
@@ -4,23 +4,16 @@ class Solution:
             return len(text)
         txt1 = ''
         txt2 = ''
-        l,r = 0,len(text)-1
+        n = len(text)
+        # l,r = 0,len(text)-1
         count = 0
-        
-        while l < r:
-            txt1 += text[l]
-            txt2 += text[r]
-            if text[r] == txt1[0]:
-                if txt1 == txt2[::-1]:
-                    txt1 = ''
-                    txt2 = ''
-                    count += 2
-            
-            l += 1
-            r -= 1
-        if l == r and len(txt1) == 0:
-            count += 1
-        if len(txt1) != 0:
-            count += 1
+        for i in range(len(text)):
+            txt1 += text[i]
+            txt2 += text[n-i-1]
+
+            if txt1 == txt2[::-1]:
+                count += 1
+                txt1 = ''
+                txt2 = ''
         
         return count
