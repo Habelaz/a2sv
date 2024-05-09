bfa81528c37df2fdbcb6037a78f977d794c42f8b
Habelaz <123306599+Habelaz@users.noreply.github.com>
2024-05-09 18:21:56 +0300

    Time: 29 ms (87.53%) | Memory: 16.5 MB (30.50%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -3,7 +3,7 @@ class Solution:
         @cache
 
         def climb(n):
-            if n <= 2:
+            if n == 2 or n == 1:
                 return n
             return climb(n-1) + climb(n-2)
         return climb(n)
