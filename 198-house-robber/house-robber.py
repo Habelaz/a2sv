265f7fad13b606cb83c54c3ec18791112e4f88ba
Habelaz <123306599+Habelaz@users.noreply.github.com>
2024-05-09 19:39:31 +0300

    Time: 36 ms (54.44%) | Memory: 16.7 MB (12.20%) - LeetSync

---
+++ 198-house-robber/house-robber.py
@@ -0,0 +1,10 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        @cache
+        def dp(i):
+            if i >= len(nums):
+                return 0
+            
+            return max(dp(i+1),nums[i] + dp(i+2))
+        
+        return dp(0)
