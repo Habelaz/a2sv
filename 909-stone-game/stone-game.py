2b517d2c8e9bb42179ffe4ee61634760f97942cf
Habelaz <123306599+Habelaz@users.noreply.github.com>
2024-05-21 18:10:25 +0300

    Time: 42 ms (65.17%) | Memory: 17.4 MB (50.37%) - LeetSync

--- 909-stone-game/stone-game.py
+++ 909-stone-game/stone-game.py
@@ -1,3 +1,14 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        return True
+        # return True
+        n = len(piles)
+        summ = sum(piles)
+        @cache
+        def dp(i,j,total,turn):
+            if j <= i:
+                return total < summ 
+            if not turn:
+                return dp(i+1,j,total + piles[i],not turn) or dp(i,j-1,total+piles[j],not turn)
+            else:
+                return dp(i+1,j,total + piles[i],turn) or dp(i,j-1,total+piles[j],turn)
+        return dp(0,n-1,0,False)
