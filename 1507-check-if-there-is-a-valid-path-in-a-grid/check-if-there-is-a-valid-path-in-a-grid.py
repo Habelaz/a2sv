2d7f38aea7b75a0b7da04f1285569a1cb0bf7512
Habelaz <123306599+Habelaz@users.noreply.github.com>
2024-06-02 14:02:34 +0300

    Time: 1075 ms (55.67%) | Memory: 56.8 MB (33.00%) - LeetSync

--- 1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.py
+++ 1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.py
@@ -1,22 +1,5 @@
 class Solution:
-    
     def hasValidPath(self, grid: List[List[int]]) -> bool:
-        def dfs(r,c):
-            if (r,c) == destination:
-                return True
-            
-            for rc,cc in directions[grid[r][c]]:
-                # print(visited)
-                nr = r + rc
-                nc = c + cc
-                if inbound(nr,nc) and (nr,nc) not in visited and [-rc,-cc] in directions[grid[nr][nc]]:
-                    visited.add((nr,nc))
-                    found = dfs(nr,nc)
-                    # print(found)
-                    if found:
-                        return True
-            return 
-
         directions = {
             1:[[0,1],[0,-1]],
             2:[[-1,0],[1,0]],
@@ -28,6 +11,23 @@ class Solution:
         
         def inbound(r,c):
             return (0 <= r < len(grid) and 0<= c < len(grid[0]))
+        visited = set()
+        visited.add((0,0))
         destination = (len(grid)-1,len(grid[0])-1)
-        visited = set([(0,0)])
+
+        def dfs(r,c):
+            if (r,c) == destination:
+                return True
+
+            for rc,cc in directions[grid[r][c]]:
+                nr = r + rc
+                nc = c + cc
+                if inbound(nr,nc) and (nr,nc) not in visited and [-rc,-cc] in directions[grid[nr][nc]]:
+                    visited.add((nr,nc))
+                    found = dfs(nr,nc)
+                    if found:
+                        return True
+            return False
+
         return dfs(0,0)
+
