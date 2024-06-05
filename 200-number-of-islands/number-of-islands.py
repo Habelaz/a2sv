66962329a82dd3a1d0927e1b32dcab54a5e82db2
Habelaz <123306599+Habelaz@users.noreply.github.com>
2024-06-05 17:06:55 +0300

    Time: 267 ms (26.42%) | Memory: 19 MB (53.52%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,31 +1,26 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        
+        visited = [[False for i in range(len(grid[0]))] for _ in range(len(grid))]
         def inbound(i,j):
             return 0 <= i < len(grid) and 0 <= j < len(grid[0])
         directions = [[1,0],[0,1],[-1,0],[0,-1]]
         def dfs(i,j):
-            stack = [(i, j)]
-    
-            while stack:
-                ci, cj = stack.pop()
-                
-                if grid[ci][cj] == '0':
-                    continue
-
-                grid[ci][cj] = '0'
-                
-                for rc, cc in directions:
-                    nr = ci + rc
-                    nc = cj + cc
-                    if inbound(nr, nc) and grid[nr][nc] == '1':
-                        stack.append((nr, nc))
+            
+            if grid[i][j] =='0':
+                return
+            
+            for rc,cc in directions:
+                nr = i + rc
+                nc = j + cc
+                if inbound(nr,nc) and not visited[nr][nc]:
+                    visited[nr][nc] = True
+                    dfs(nr,nc)
             return
         
         count = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == '1':
+                if grid[i][j] == '1' and not visited[i][j]:
                     dfs(i,j)
                     count += 1
 
