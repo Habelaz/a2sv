41a2e704686d798c1faae2e1bd70199dfdcc98a6
Habelaz <123306599+Habelaz@users.noreply.github.com>
2024-06-24 17:47:17 +0300

    Time: 86 ms (14.03%) | Memory: 17.8 MB (83.87%) - LeetSync

---
+++ 402-remove-k-digits/remove-k-digits.py
@@ -0,0 +1,17 @@
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        stack = []
+        count = 0
+        
+        for n in num:
+            while stack and int(stack[-1]) > int(n) and count < k:
+                stack.pop()
+                count += 1
+            stack.append(n)
+        
+        while count < k:
+            stack.pop()
+            count += 1
+        
+        ans = ''.join(stack).lstrip('0')  # Remove leading zeros
+        return ans if ans else '0'
