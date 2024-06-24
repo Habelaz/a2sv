aae8502cd6200843dcf8f9ad7046f7083973aaf9
Habelaz <123306599+Habelaz@users.noreply.github.com>
2024-06-24 14:01:59 +0300

    Time: 44 ms (82.56%) | Memory: 16.6 MB (94.41%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,12 +1,12 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         stack = []
-        mapp = defaultdict(lambda :-1)
+        maxx = max(nums1) + 1
+        greater = defaultdict(lambda : -1)
 
         for n in nums2:
             while stack and stack[-1] < n:
-                x = stack.pop()
-                mapp[x] = n
+                greater[stack.pop()] = n 
             stack.append(n)
         
-        return [mapp[val] for val in nums1 ]
+        return [greater[n] for n in nums1]
