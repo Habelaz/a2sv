f780973258ea3e63c3a0c095d80284652e260557
Habelaz <123306599+Habelaz@users.noreply.github.com>
2024-08-11 12:47:25 +0300

    Time: 71 ms (82.26%) | Memory: 17.8 MB (33.67%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,33 +1,31 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        # l,r = 0,len(nums)-1
+        
         def first(left,right):
             while left <= right:
                 mid = left + (right - left) // 2
                 if nums[mid] == target:
-                    if mid == 0 or  nums[mid - 1] != target:
+                    if mid == 0 or nums[mid -1] != target:
                         return mid
-                    else:
-                        right = mid - 1
-                elif nums[mid] > target:
                     right = mid - 1
+                elif nums[mid] < target:
+                    left = mid +  1
                 else:
-                    left = mid + 1
+                    right = mid - 1
             return -1
 
         def last(left,right):
             while left <= right:
                 mid = left + (right - left) // 2
                 if nums[mid] == target:
-                    if mid == len(nums) - 1 or nums[mid+1] != target:
+                    if mid == len(nums) - 1 or nums[mid + 1] != target:
                         return mid
-                    else:
-                        left = mid + 1
-                elif nums[mid] > target:
-                    right = mid - 1
-                else:
                     left = mid + 1
+                elif nums[mid] < target:
+                    left = mid +  1
+                else:
+                    right = mid - 1
             return -1
-        
-        return [first(0,len(nums)-1),last(0,len(nums)-1)]
-        
+
+        left,right = 0,len(nums) - 1
+        return [first(left,right),last(left,right)]
