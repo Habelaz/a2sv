88355b3c31b652e0bbeb66f2f2b20fc5b1a82e06
Habelaz <123306599+Habelaz@users.noreply.github.com>
2024-10-14 19:25:22 +0300

    Time: 725 ms (60.76%) | Memory: 31.4 MB (20.57%) - LeetSync

---
+++ 2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxKelements(self, nums: List[int], k: int) -> int:
+        nums = [-n for n in nums]
+        heapq.heapify(nums)
+        ans = 0
+        for i in range(k):
+            num = heapq.heappop(nums)
+            ans += -num
+            heapq.heappush(nums,-ceil(-num/3))
+            # print(nums,-num)
+            
+        
+        return ans
