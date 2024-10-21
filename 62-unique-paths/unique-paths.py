0268ec149c1fef65c0aaffb4a5a035514d8d1f6c
Habelaz <123306599+Habelaz@users.noreply.github.com>
2024-10-21 18:45:58 +0300

    Time: 0 ms (100.00%) | Memory: 16.8 MB (11.38%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,28 +1,3 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        
-        # @cache
-        # def dp(i,j):
-        #     if i >= m or j >= n:
-        #         return 0
-        #     if i == m-1 and j == n-1:
-        #         return 1
-
-        #     return dp(i+1,j) + dp(i,j+1)
-
-        # return dp(0,0)
-
-        dp = [[0 for i in range(n)] for j in range(m)]
-
-        dp[-1][-1] = 1
-
-        def inbound(i,j):
-            if 0 <=i < m and 0 <= j < n:
-                return dp[i][j]
-            return 0
-
-        for i in range(m-1,-1,-1):
-            for j in range(n-1,-1,-1):
-                dp[i][j] += inbound(i+1,j) + inbound(i,j+1)
-
-        return dp[0][0]
+        return math.comb(m+n-2,n-1)
