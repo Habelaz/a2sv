311b2abf67d5d4dbd1a4f910509706159fee498b
Abel Zereabruk <123306599+Habelaz@users.noreply.github.com>
2025-02-04 10:55:08 +0300

    Time: 0 ms (100.00%) | Memory: 4 MB (57.69%) - LeetSync

---
+++ 1927-maximum-ascending-subarray-sum/maximum-ascending-subarray-sum.go
@@ -0,0 +1,16 @@
+func maxAscendingSum(nums []int) int {
+    ans := nums[0]
+    sum := nums[0]
+
+    for i := 1; i < len(nums); i++ {
+        if nums[i] > nums[i-1] {
+            sum += nums[i]
+            ans = int(math.Max(float64(ans), float64(sum)))
+        }else{
+            ans = int(math.Max(float64(ans), float64(sum)))
+            sum = nums[i]
+        }
+
+    }
+    return ans
+}
